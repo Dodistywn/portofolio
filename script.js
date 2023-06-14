5e8014d94763f6625ac3b78b103b668aa79b7383
Dodistywn <dodisetya000@gmail.com>
2023-06-14 15:14:06 +0700

    add images and add view home

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ MenuBtn.addEventListener('click', function(e){
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark')
 })
+
+
+// effect
+
+let typed = new Typed('.auto-input',{
+    strings: ['Front-End Developer!', 'Web Developer!', 'Designer!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
