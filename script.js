5bd26b5773b80329e9171ec91ef003b87864952a
Dodistywn <dodisetya000@gmail.com>
2023-06-14 13:59:27 +0700

    remove project sebelumnya change with new view, add view header= profil, navbar, footer

---
+++ script.js
@@ -0,0 +1,8 @@
+// Header toogle
+
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click', function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
